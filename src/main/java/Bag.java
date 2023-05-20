289ec06739bfeedfc944be4e00be90308ce46fa7
= <lilyxmeng13@gmail.com>
2023-05-20 19:53:21 -0400

    work

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -13,9 +13,10 @@ public abstract class Bag {
      *       - an int named capacity
      *       - an array of Strings named contents
      */
-
-
-
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
     /*
      * TODO: Create a constructor that takes two arguments:
@@ -26,9 +27,12 @@ public abstract class Bag {
      * be empty (e.g. numberOfContents is 0 and an empty String array for
      * its contents.)
      */
-
-
-
+    public Bag(String color, int capacity) {
+        this.color = color;
+        this.numberOfContents = 0;
+        this.capacity = capacity;
+        this.contents = new String[capacity];
+    }
 
     /*
      * TODO: Create a variety of 'getter' functions.
@@ -37,18 +41,25 @@ public abstract class Bag {
      *           - getNumberOfContents
      *           - getCapacity
      */
+    public String getColor() {
+        return this.color;
+    }
 
+    public int getNumberOfContents() {
+        return this.numberOfContents;
+    }
 
-
+    public int getCapacity() {
+        return this.capacity;
+    }
 
     /*
      * TODO: Create a setter function called setColor which sets the
      *       color of this bag to the given color.
      */
-
-
-
-
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     /*
      * TODO: Create a method called addItem that takes in a String
@@ -60,10 +71,14 @@ public abstract class Bag {
      *       This method should return true if the item was added
      *       and false otherwise.
      */
-
-
-
-
+    public Boolean addItem(String item) {
+        if (this.numberOfContents <= this.capacity) {
+            this.contents[numberOfContents] = item;
+            this.numberOfContents++;
+            return true;
+        }
+        return false;
+    }
 
     /**
      * TODO: Create a method called popItem that returns a String.
@@ -73,12 +88,17 @@ public abstract class Bag {
      *
      * If there are no items in this Bag, return null.
      *
-     * @return
+     * @return The last item added to this bag.
      */
-
-
-
-
+    public String popItem() {
+        if (this.numberOfContents >= 0) {
+            this.numberOfContents--;
+            String item = this.contents[this.numberOfContents];
+            this.contents[this.numberOfContents] = null;
+            return item;
+        }
+        return null;
+    }
 
     /**
      * Increase this bag's capacity by n.
@@ -86,8 +106,18 @@ public abstract class Bag {
      * @param n the amount to increase this Bag's capacity by
      */
     public void increaseCapacity(int n) {
-        // TODO: Implement this method.
+        this.capacity += n;
 
+        String[] prevContents = this.contents;
+        this.contents = new String[this.capacity];
+
+        this.numberOfContents = 0;
+        for (String item : prevContents) {
+            if (item == null) {
+                return;
+            }
+            addItem(item);
+        }
     }
 
     /**
@@ -95,7 +125,7 @@ public abstract class Bag {
      * This method requires you to have created the private
      * instance variables mentioned above.
      *
-     * @return
+     * @return The details of this bag.
      */
     @Override
     public String toString() {
--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -30,3 +30,26 @@
  *
  *       See the tests in test/CrossBodyBagTest.java for examples.
  */
+public class CrossbodyBag extends Bag {
+    int numberOfStraps;
+
+    public CrossbodyBag(String color, int capacity, int numberOfStraps) {
+        super(color, capacity);
+        this.numberOfStraps = numberOfStraps;
+    }
+
+    public int getNumberOfStraps() {
+        return this.numberOfStraps;
+    }
+
+    @Override
+    public void enhance() {
+        this.increaseCapacity(2);
+    }
+
+    @Override
+    public String toString() {
+        return getColor() + " Crossbody Bag with " + this.numberOfStraps + " straps (" + getNumberOfContents() + " / " +
+                this.getCapacity() + ")";
+    }
+}
